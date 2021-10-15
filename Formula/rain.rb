011a3ac0631b793b7506c84dcf1d65dd7690ff29
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-14 22:58:22 -0400

    Brew formula update for rain version v1.7.2

--- Formula/rain.rb
+++ Formula/rain.rb
@@ -5,20 +5,20 @@
 class Rain < Formula
   desc "BitTorrent client"
   homepage "https://github.com/cenkalti/rain"
-  version "1.7.1"
+  version "1.7.2"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/cenkalti/rain/releases/download/v1.7.1/rain_1.7.1_macos.tar.gz"
-      sha256 "d5383bf85c1cc14da56e43a4fd9f45cba0583287c6919e8db24217e4754f614d"
+      url "https://github.com/cenkalti/rain/releases/download/v1.7.2/rain_1.7.2_macos.tar.gz"
+      sha256 "b27ba823c90f6146feba6a0d53e079cf9904d842219f99b29f3b16d60b8ba2e3"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/cenkalti/rain/releases/download/v1.7.1/rain_1.7.1_linux.tar.gz"
-      sha256 "87bc3916c78f3ce851a2a69265412c164ee85439c9dc60c043d659773de6d4a2"
+      url "https://github.com/cenkalti/rain/releases/download/v1.7.2/rain_1.7.2_linux.tar.gz"
+      sha256 "43f1bdde7813cc1bb9554d25a7d8e870a66963c01d5b697577e1dad5c5f658e4"
     end
   end
 
