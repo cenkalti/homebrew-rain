f568280ea627b2e8b26fcffd6543447e3fb3298b
goreleaserbot <goreleaser@carlosbecker.com>
2022-04-17 22:41:56 -0400

    Brew formula update for rain version v1.8.7

--- Formula/rain.rb
+++ Formula/rain.rb
@@ -5,26 +5,34 @@
 class Rain < Formula
   desc "BitTorrent client"
   homepage "https://github.com/cenkalti/rain"
-  version "1.8.6"
+  version "1.8.7"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/cenkalti/rain/releases/download/v1.8.6/rain_1.8.6_macos.tar.gz"
-      sha256 "2b44c5d8795f98497bf192c76322c568cd0d33ef9273382494a355c4d0991c5b"
+    url "https://github.com/cenkalti/rain/releases/download/v1.8.7/rain_1.8.7_macos.tar.gz"
+    sha256 "aa607a84220fd176ea22b934ed755397bdbf9f0671e4a55a62365be5a423439c"
 
-      def install
-        bin.install "rain"
-        output = Utils.popen_read("#{bin}/rain bash-autocomplete")
-        (bash_completion/"rain").write output
-        prefix.install_metafiles
+    def install
+      bin.install "rain"
+      output = Utils.popen_read("#{bin}/rain bash-autocomplete")
+      (bash_completion/"rain").write output
+      prefix.install_metafiles
+    end
+
+    if Hardware::CPU.arm?
+      def caveats
+        <<~EOS
+          The darwin_arm64 architecture is not supported for the Rain
+          formula at this time. The darwin_amd64 binary may work in compatibility
+          mode, but it might not be fully supported.
+        EOS
       end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/cenkalti/rain/releases/download/v1.8.6/rain_1.8.6_linux.tar.gz"
-      sha256 "f41d9d50e1d17ca6a5b9b78f871c406914a4ec81b0da12913095a68fca543c2f"
+      url "https://github.com/cenkalti/rain/releases/download/v1.8.7/rain_1.8.7_linux.tar.gz"
+      sha256 "774004cbe3ea5b8c0dcbed71a949917a3fe6440b83da88054a3ca6a58e7ebe90"
 
       def install
         bin.install "rain"
